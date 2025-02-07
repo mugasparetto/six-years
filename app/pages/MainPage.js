ef575d42a3c07c96fdea5de27c299c4f19d43d35
mugasparetto <mu.gasparetto@hotmail.com>
2025-02-07 16:56:21 +0000

    setting up highlight animation

--- app/pages/MainPage.js
+++ app/pages/MainPage.js
@@ -7,10 +7,26 @@ class MainPage extends Page {
       element: ".main-page",
       elements: { wrapper: ".main-page__content" },
     });
+
+    this.isShowingYears = false;
   }
 
   create() {
     super.create();
+  }
+
+  update() {
+    super.update();
+
+    if (this.scroll.current > 0 && !this.isShowingYears) {
+      console.log("animate out title");
+      this.isShowingYears = true;
+    }
+
+    if (this.scroll.current <= 0 && this.isShowingYears) {
+      console.log("animate in title");
+      this.isShowingYears = false;
+    }
   }
 }
 
